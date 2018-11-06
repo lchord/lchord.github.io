5e20c598ee5e513882f001897930d2a5f6bb6fd6
lchord <569187749@qq.com>
2018-11-06 20:09:21 +0800

    clean

--- js/find.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $(".nav-toggle").click(function(){
-        $(this).toggleClass("is-active");
-        $("#menu").toggleClass("menu-iv");
-        $("#menu").toggleClass("menu-show");
-    });
-  });
